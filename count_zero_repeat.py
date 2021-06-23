e35940d10c2393c4aade3d39b7448624b43aa56c
alicecsq <51415436+alicecsq@users.noreply.github.com>
2021-06-23 14:43:27 -0400

    Update count_zero_repeat.py

--- count_zero_repeat.py
+++ count_zero_repeat.py
@@ -7,17 +7,21 @@ import sys
 
 def parse_args():
     '''Parses arguments.'''
-    parser = argparse.ArgumentParser(description='calculate % total R2C2 reads with zero repeats.',
-                                     add_help=True,
-                                     prefix_chars='-')
-    parser.add_argument('--input_R2C2_Consensus_file', '-i', type=str, action='store',
-                        help='Fasta file with consensus called R2C2 reads (output from C3POa.py)'))
+    
+    
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(0)
     return parser.parse_args()
 args = parse_args()
-inFile =args.input_R2C2_Consensus_file
+parser = argparse.ArgumentParser(description='calculate % total R2C2 reads with zero repeats.',
+                                     add_help=True,
+                                     prefix_chars='-')
+parser.add_argument('--input_file', '-i', type=str, action='store',
+                        help='Fasta file with consensus called R2C2 reads (output from C3POa.py)'))
+
+args=parser.parse_args()
+inFile =args.input_file
 
 def zero_repeat(inFile):
   read_count = 0
